cc57d39b58c394423ee962910f24d610648ae68c
sah-abhishek <abhisheksah3333@gmail.com>
2025-07-23 12:29:14 +0530

    Pwa

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,31 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'My React PWA',
+      short_name: 'ReactPWA',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#1a202c',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
